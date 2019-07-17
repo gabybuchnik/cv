65dcd8ece474076b2e2fafae82f2b0bd230792a1
gabybuchnik <gabybuchnik@gmail.com>
2019-07-17 13:24:26 +0300

    add components

--- client/mycv/src/app/app-routing.module.ts
+++ client/mycv/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AboutComponent} from './components/about/about.component';
+import {WorksComponent} from './components/works/works.component';
+import {SkillsComponent} from './components/skills/skills.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'' , component : AboutComponent , pathMatch : 'full'},
+  {path:'about' , component : AboutComponent },
+  {path:'works' , component : WorksComponent },
+  {path:'skills' , component : SkillsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/mycv/src/app/app.module.ts
+++ client/mycv/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HeaderComponent } from './components/about/header/header.component';
+import { ResumeComponent } from './components/about/resume/resume.component';
+import { FooterComponent } from './components/about/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+import { WorksComponent } from './components/works/works.component';
+import { SkillsComponent } from './components/skills/skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    ResumeComponent,
+    FooterComponent,
+    AboutComponent,
+    WorksComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
