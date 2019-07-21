9ff194fa0e1abc1d51d7a8d4bec9b722a042b1ff
gabybuchnik <gabybuchnik@gmail.com>
2019-07-21 15:11:02 +0300

    add skills

--- client/mycv/src/app/components/skills/skills.component.ts
+++ client/mycv/src/app/components/skills/skills.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.css']
 })
-export class SkillsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class SkillsComponent implements OnInit, AfterViewChecked {
+  bar1: number;
+  bar2: number;
+  bar3: number;
+  constructor() {
+    this.bar1 = null;
+    this.bar2 = null;
+    this.bar3 = null;
   }
-
+  ngOnInit() {
+    this.bar1 = 0;
+    this.bar2 = 0;
+    this.bar3 = 0;
+  }
+  ngAfterViewChecked() {
+    setTimeout(()=>{
+      this.bar1 = 100;
+      this.bar2 = 95;
+      this.bar3 = 85;
+    },100) 
+  }
 }
