c91e05cb5fc8a643fa5d5c93d6ed4b0211add1b5
gabybuchnik <gabybuchnik@gmail.com>
2019-07-21 15:57:16 +0300

    fix skills css

--- client/mycv/src/app/components/skills/skills.component.ts
+++ client/mycv/src/app/components/skills/skills.component.ts
@@ -9,21 +9,32 @@ export class SkillsComponent implements OnInit, AfterViewChecked {
   bar1: number;
   bar2: number;
   bar3: number;
+  bar4: number;
+  bar5: number;
+  bar6: number;
   constructor() {
     this.bar1 = null;
     this.bar2 = null;
     this.bar3 = null;
+    this.bar4 = null;
+    this.bar5 = null;
   }
   ngOnInit() {
     this.bar1 = 0;
     this.bar2 = 0;
     this.bar3 = 0;
+    this.bar4 = 0;
+    this.bar5 = 0;
+    this.bar6 = 0;
   }
   ngAfterViewChecked() {
-    setTimeout(()=>{
+    setTimeout(() => {
       this.bar1 = 100;
       this.bar2 = 95;
       this.bar3 = 85;
-    },100) 
+      this.bar4 = 85;
+      this.bar5 = 50;
+      this.bar6 = 40;
+    }, 100)
   }
 }
