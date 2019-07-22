ab29ff43e8237636eca42f99c4358bf32d7b32b7
gabybuchnik <gabybuchnik@gmail.com>
2019-07-22 12:11:48 +0300

    deploy

--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ const express  = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+
 const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(port , ()=> console.log(`listen to port -  ${port}`));
+app.listen(port , ()=> console.log(`listen to port  ${port}`));
