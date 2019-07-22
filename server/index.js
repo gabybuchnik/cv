09031e32480dd9874730c16bcfcb167cc2899905
gabybuchnik <gabybuchnik@gmail.com>
2019-07-22 12:07:56 +0300

    fix

--- server/index.js
+++ server/index.js
@@ -6,4 +6,4 @@ const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(port , ()=> console.log(`listen to port ${port}`));
+app.listen(port , ()=> console.log(`listen to port -  ${port}`));
