7efeedc6d6e867aa9d2b260c11b4d5cb13a11a4e
gabybuchnik <gabybuchnik@gmail.com>
2019-07-23 12:16:31 +0300

    fix

--- client/mycv/src/app/app.component.ts
+++ client/mycv/src/app/app.component.ts
@@ -9,6 +9,6 @@ import { Router} from '@angular/router';
 export class AppComponent implements OnInit{
   constructor(private router : Router){}
   ngOnInit(){
-    this.router.navigate(['/', 'about']);
+    this.router.navigate(['./', 'about']);
   }
 }
